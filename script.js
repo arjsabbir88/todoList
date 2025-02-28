dbfddb88539ae29db2fa0e0455865b5c70463aa6
sabbir88 <arjsabbir88@gmail.com>
2025-02-28 16:50:33 +0600

    now added teh validation

--- script.js
+++ script.js
@@ -2,9 +2,23 @@ const inputField = document.getElementById("input-field");
 const toDoBtn = document.getElementById("toDoBtn");
 const addedTask = document.getElementById("addedTask");
 toDoBtn.addEventListener("click", function(){
-    const createElement = document.createElement("h2");
-    createElement.textContent = inputField.value;
-    createElement.classList = "border-b-2 border-slate-800 p-2 text-xl"
-    addedTask.appendChild(createElement);
-    inputField.value = "";
+
+    if(inputField.value == ""){
+        alert("please write something");
+    }else{
+        const createElement = document.createElement("h2");
+        createElement.textContent = inputField.value;
+        addedTask.appendChild(createElement);
+        const i = document.createElement("i");
+        i.classList = "fa-solid fa-xmark cursor-pointer text-red-500";
+        createElement.appendChild(i);
+        createElement.classList = "border-b-2 border-slate-800 p-2 text-xl"
+        inputField.value = "";
+        
+
+        i.addEventListener("click", function(){
+            addedTask.removeChild(createElement);
+        })
+    }
+
 })
